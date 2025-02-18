98fed3dbe5bb2ffa82e05d59cb39eab4764f8be8
ALTERRA4546 <kordroy20@mail.ru>
2025-02-18 21:05:25 +0300

    1

---
+++ Билет 16/Ticket 16/Ticket 16/Ticket 16.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+class CollectionOperations {
+public:
+    // Добавление элемента в коллекцию
+    static void addElement(std::vector<int>& collection, int element) {
+        collection.push_back(element);
+    }
+
+    // Удаление элемента из коллекции
+    static void removeElement(std::vector<int>& collection, int element) {
+        auto it = std::find(collection.begin(), collection.end(), element);
+        if (it != collection.end()) {
+            collection.erase(it);
+        }
+    }
+
+    // Поиск элемента в коллекции
+    static bool containsElement(const std::vector<int>& collection, int element) {
+        return std::find(collection.begin(), collection.end(), element) != collection.end();
+    }
+
+    // Получение размера коллекции
+    static size_t getSize(const std::vector<int>& collection) {
+        return collection.size();
+    }
+
+    // Очистка коллекции
+    static void clearCollection(std::vector<int>& collection) {
+        collection.clear();
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ Билет 16/Ticket 16/UnitTest/UnitTest.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include "C:\Users\King Night\Desktop\Тестирование билеты\Билет 16\Ticket 16\Ticket 16\Ticket 16.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+    TEST_CLASS(UnitTest) {
+public:
+
+    TEST_METHOD(AddElementTest) {
+        std::vector<int> collection;
+        CollectionOperations::addElement(collection, 5);
+        Assert::AreEqual((size_t)1, CollectionOperations::getSize(collection));
+        Assert::IsTrue(CollectionOperations::containsElement(collection, 5));
+    }
+
+    TEST_METHOD(RemoveElementTest) {
+        std::vector<int> collection;
+        CollectionOperations::addElement(collection, 5);
+        CollectionOperations::addElement(collection, 10);
+        CollectionOperations::removeElement(collection, 5);
+        Assert::AreEqual((size_t)1, CollectionOperations::getSize(collection));
+        Assert::IsFalse(CollectionOperations::containsElement(collection, 5));
+        Assert::IsTrue(CollectionOperations::containsElement(collection, 10));
+    }
+
+    TEST_METHOD(ContainsElementTest) {
+        std::vector<int> collection;
+        CollectionOperations::addElement(collection, 5);
+        Assert::IsTrue(CollectionOperations::containsElement(collection, 5));
+        Assert::IsFalse(CollectionOperations::containsElement(collection, 10));
+    }
+
+    TEST_METHOD(GetSizeTest) {
+        std::vector<int> collection;
+        CollectionOperations::addElement(collection, 5);
+        CollectionOperations::addElement(collection, 10);
+        Assert::AreEqual((size_t)2, CollectionOperations::getSize(collection));
+    }
+
+    TEST_METHOD(ClearCollectionTest) {
+        std::vector<int> collection;
+        CollectionOperations::addElement(collection, 5);
+        CollectionOperations::clearCollection(collection);
+        Assert::AreEqual((size_t)0, CollectionOperations::getSize(collection));
+    }
+
+    TEST_METHOD(RemoveNonExistingElementTest) {
+        std::vector<int> collection;
+        CollectionOperations::addElement(collection, 5);
+        CollectionOperations::removeElement(collection, 10);
+        Assert::AreEqual((size_t)1, CollectionOperations::getSize(collection));
+        Assert::IsTrue(CollectionOperations::containsElement(collection, 5));
+    }
+    };
+}
